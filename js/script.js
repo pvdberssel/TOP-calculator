03e318068064e2d9ef302fdb6c33967a9b0dea45
pvdberssel <pim.bersselaar@live.com>
2024-10-09 14:41:28 +0200

    Add basic math operations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log('link test')
+function add(a,b){
+    return a+b;
+}
+
+function substract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber,secondNumber, operator){
+    switch(operator){
+        case '+':
+            add(firstNumber,secondNumber);
+            break;
+        case '-':
+            substract(firstNumber,secondNumber);
+            break;
+        case '*':
+            multiply(firstnumber,secondNumber);
+            break;
+        case '/':
+            divide(firstNumber,secondNumber);
+            break;
+    }
+}

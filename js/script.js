7123f0e5d59cb506634378f3a1eb6803ef1b49a2
pvdberssel <pim.bersselaar@live.com>
2024-10-09 17:02:04 +0200

    Add onclick event equal button

--- js/script.js
+++ js/script.js
@@ -29,6 +29,21 @@ numberButtons.forEach((button) => {
     })
 })
 
+const clearButton = document.querySelector('.clear');
+
+clearButton.addEventListener('click', () => {
+        console.log('click');
+        const screen=document.querySelector('.screen');
+        screen.textContent='0';
+})
+
+const equalButton = document.querySelector('.equals');
+
+equalButton.addEventListener('click', () => {
+        console.log('CALCULATE');
+        const screen=document.querySelector('.screen');
+        screen.textContent='0';
+})
 
 
 
